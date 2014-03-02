381eb95d50f5bf20102bd248da2199badbc3a071
kegamin <keitti05@gmail.com>
2014-03-02 19:08:29 +0900

    added

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-DOT_FILES=( .bashrc .bashrc_perlbrew .bashrc_rbenv .bashrc_git)
+
+# -------------------------------------------------
+# symlink dotfiles
+
+DOT_FILES=( .bash_profile .bashrc .inputrc .vimrc .vim )
 BACKUP=$HOME/dotbackup
 DATE=`date +"%Y%m%d%k%M%S"`
 
@@ -22,7 +26,32 @@ do
   echo "シンボリックリンク作成 : $file"
 done
 
-#if [ ! -d ~/.vim/bundle ]; then
-#  mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && vim -c ':NeoBundleInstall'
-#fi
+# -------------------------------------------------
+# install NeoBundle
+if [ ! -d ~/.vim/bundle ]; then
+  mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && vim -c ':NeoBundleInstall'
+fi
+
+# -------------------------------------------------
+# OS独自setup定義  TODO:インストール系はMakefileに移したい
+
+if [ `uname` = "Darwin" ]; then
+  brew=/usr/local/bin
+  if [ ! -f $brew/brew ]; then
+    echo "install brew"
+    sudo mkdir $brew
+    ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
+    type brew >/dev/null 2>&1 && export PATH=$brew:${PATH//$brew:/}
+  fi
+
+  if [ ! -f $(brew --prefix)/etc/bash_completion ]; then
+    echo "install bash-completion"
+    brew install bash-completion
+  fi
+  if [ -f $(brew --prefix)/etc/bash_completion -a ! -f $HOME/.bash_completion ]; then
+    file=$(brew --prefix)/etc/bash_completion
+    ln -s $file $HOME/.bash_completion
+    echo "シンボリックリンク作成 : .bash_completion"
+  fi
+fi
 
