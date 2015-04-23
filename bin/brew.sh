b20e6aff02ef223b3dc8056e995661aec125a3af
kegamin <keitti05@gmail.com>
2015-04-23 16:01:26 +0900

    新規macbook air追加時にエラーになってた部分を修正

--- bin/brew.sh
+++ bin/brew.sh
@@ -10,6 +10,7 @@ brew tap phinze/homebrew-cask
 brew install ack
 brew install ag
 brew install autoconf
+brew install automake
 brew install git
 brew install gist
 brew install hub
@@ -26,11 +27,12 @@ brew install fontforge
 brew install reattach-to-user-namespace
 brew install byobu
 brew install readline
-brew install ctags
+# brew install ctags
 brew install node
 brew install z
 brew install htop
 brew install unrar
+brew install pkg-config
 
 # GNU tools
 brew install xz
@@ -49,14 +51,18 @@ brew install mongodb
 
 # macvimを入れる
 brew tap supermomonga/homebrew-splhack
+brew install cscope
+brew install lua
 brew install --HEAD cmigemo-mk
 brew install --HEAD ctags-objc-ja
 brew install gettext-mk
-brew install --HEAD macvim-kaoriya
+brew link ctags-objc-ja
+brew install macvim-kaoriya --HEAD --with-lua　--with-cscope
 brew linkapps
 
 # powerline用フォントを作成する
 brew tap sanemat/font
+brew install Caskroom/cask/xquartz
 brew install --powerline --vim-powerline ricty
 
 # 以下mac用アプリを入れる
@@ -65,14 +71,13 @@ brew cask install google-chrome
 brew cask install bettertouchtool
 brew cask install sourcetree
 brew cask install dash
-brew cask install alfred link
+brew cask install alfred
 brew cask install kobito
 brew cask install skype
 brew cask install dropbox
 brew cask install iterm2
 brew cask install vagrant
 brew cask install virtualbox
-brew cask install alfred
 brew cask install github
 brew cask install firefox
 brew cask install insomniax
@@ -80,3 +85,12 @@ brew cask install atom
 
 # remove outdated versions
 brew cleanup
+
+# installing ricty fonts
+if [ -d /usr/local/Celler/ricty/3.2.3/share/fonts ]; then
+  # rictyフォントをコピーして,フォントキャッシュをクリアする
+  cp -f /usr/local/Cellar/ricty/3.2.3/share/fonts/Ricty*.ttf ~/Library/Fonts/ && fc-cache -vf
+  defaults -currentHost write -globalDomain AppleFontSmoothing -int 2
+else
+    echo "rictyのフォントが未インストール."
+fi
