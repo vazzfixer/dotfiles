cbc8ba4daa9dac3c0edaf38c54e2d99153c30ed4
kegamin <keitti05@gmail.com>
2015-05-09 18:06:30 +0900

    pandocのplantuml pluginを追加インストール

--- homebrew/install_pandoc.sh
+++ homebrew/install_pandoc.sh
@@ -3,13 +3,24 @@
 # HomebrewではなくHaskelのパッケージ管理cabal
 # pandocを利用して、markdownをPDF/html化するための準備
 brew install ghc cabal-install
+brew install graphviz
+brew install plantuml
+brew cask install basictex
+
 cabal update
 cabal install cabal-install
 cabal install pandoc
-brew cask install basictex
+
 sudo tlmgr update --self
 sudo tlmgr update --all
 sudo tlmgr install collection-langjapanese collection-latexrecommended collection-fontsrecommended
 
 # remove outdated versions
 brew cleanup
+
+# For use plantuml in markdown.
+echo "installing pandoc-plantuml----"
+cd /tmp
+git clone git@github.com:kbonne/pandoc-plantuml-filter.git
+cd pandoc-plantuml-filter/
+cabal install
