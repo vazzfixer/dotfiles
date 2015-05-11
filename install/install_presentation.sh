defdd7252617172cfe967d2c37a322808ee80b79
kegamin <keitti05@gmail.com>
2015-05-11 16:15:33 +0900

    pluginを追加

--- install/install_presentation.sh
+++ install/install_presentation.sh
@@ -3,7 +3,13 @@
 # rubyのpresentation作成ツール
 # 使い方
 # $ slideshow build memo.md
+# $ wkhtmltopdf -O Landscape memo.pdf.html slides.pdf
+
 
 gem install slideshow
 brew install wkhtmltopdf
 
+slideshow intall shower
+# s6blank, s6syntax, s5blank, s5themes, g5, slidy, deck.js, impress.js, analytics
+# $ slideshow list #>一覧表示
+slideshow install -a
