af29d5f3c2e98a76940ec107c2fdbc5f70178bb5
kegamin <keitti05@gmail.com>
2015-07-16 16:47:43 +0900

    dotfilesで標準的に使うvitalをhttps://github.com/b4b4r07/dotfiles/blob/master/etc/install からコピー

--- etc/lib/vital.sh
+++ etc/lib/vital.sh
@@ -1,63 +1,238 @@
-#!/bin/sh
 #!/bin/bash
-#!/bin/zsh
+#                                    
+#         _ _        _       _       
+#        (_) |      | |     | |      
+#  __   ___| |_ __ _| |  ___| |__    
+#  \ \ / / | __/ _` | | / __| '_ \   
+#   \ V /| | || (_| | |_\__ \ | | |  
+#    \_/ |_|\__\__,_|_(_)___/_| |_|  
+#                                    
+#                                    
 
-# SHELL
-is_bash() { [[ -n $BASH_VERSION ]]; }
-is_zsh() { [[ -n $ZSH_VERSION ]]; }
+export PLATFORM
 
-# PLATFORM
-export SHELL_PLATFORM='unknown'
-ostype() { echo "$OSTYPE" | tr '[:upper:]' '[:lower:]'; }
-case "$(ostype)" in
-    *'linux'*)  SHELL_PLATFORM='linux' ;;
-    *'darwin'*) SHELL_PLATFORM='osx'   ;;
-    *'bsd'*)    SHELL_PLATFORM='bsd'   ;;
-esac
-is_linux() { [[ $SHELL_PLATFORM == 'linux' || $SHELL_PLATFORM == 'bsd' ]]; }
-is_osx()   { [[ $SHELL_PLATFORM == 'osx' ]]; }
-is_bsd()   { [[ $SHELL_PLATFORM == 'bsd' || $SHELL_PLATFORM == 'osx' ]]; }
+readonly _TAB_="$(printf "\t")"
+readonly _SPACE_=' '
+readonly _BLANK_="${_SPACE_}${_TAB_}"
+readonly _IFS_="$IFS"
 
-# Check if exists
-is_exist() { type "$1" >/dev/null 2>&1; return $?; }
-is_exists() {
-    if [[ ${1[1,1]} =~ / ]]; then
-        if [[ -e "$1" ]]; then
-            return 0
-        fi
+# Gerenal utilities {{{1
+e_newline() {
+    printf "\n"
+}
+
+e_header() {
+    printf " \033[37;1m%s\033[m\n" "$*"
+}
+
+e_error() {
+    printf " \033[31m%s\033[m\n" "✖ $*" 1>&2
+}
+
+e_done() {
+    printf " \033[37;1m%s\033[m...\033[32mOK\033[m\n" "✔ $*"
+}
+
+e_arrow() {
+    printf " \033[37;1m%s\033[m\n" "➜ $*"
+}
+
+e_indent() {
+    for ((i=0; i<${1:-4}; i++)); do
+        echon " "
+    done
+    if [ -n "$2" ]; then
+        echo "$2"
     else
-        if type "$1" >/dev/null 2>&1; then
-            return 0
-        fi
+        cat <&0
     fi
-    return 1
 }
 
-# Whether first login shell
-is_login_shell() { [[ $SHLVL == 1 ]]; }
-
-# Test whether we're in a git repo
-is_git_repo() { git rev-parse --is-inside-work-tree &>/dev/null; }
-
-# Check if running the screen or tmux
-is_screen_running() { [ ! -z "$STY" ]; }
-is_tmux_runnning() { [ ! -z "$TMUX" ]; }
-is_screen_or_tmux_running() { is_screen_running || is_tmux_runnning; }
-shell_has_started_interactively() { [ ! -z "$PS1" ]; }
-is_ssh_running() { [ ! -z "$SSH_CONECTION" ]; }
-
-# Check if exists and sourcing file
-import() { eval "[[ -${2:-f} $1 ]]" && source "$1"; }
-
-cwd() {
-    cd "$(dirname "${1:-${BASH_SOURCE:-$0}}")" && pwd
+e_success() {
+    printf " \033[37;1m%s\033[m%s...\033[32mOK\033[m\n" "✔ " "$*"
 }
 
-has() {
-    type "$1" >/dev/null 2>&1
+e_failure() {
+    die "${1:-$FUNCNAME}"
+}
+
+# is_exists returns true if executable $1 exists in $PATH
+is_exists() {
+    which "$1" >/dev/null 2>&1
     return $?
 }
 
-is_help() {
-    [[ "${1:-}" == "-h" || "${1:-}" == "--help" ]]
+# has is wrapper function
+has() {
+    is_exists "$@"
+}
+
+# die returns exit code error and echo error message
+die() {
+    e_error "$1" 1>&2
+    exit "${2:-1}"
+}
+
+# is_login_shell returns true if current shell is first shell
+is_login_shell() {
+    [ "$SHLVL" = 1 ]
+}
+
+# is_git_repo returns true if cwd is in git repository
+is_git_repo() {
+    git rev-parse --is-inside-work-tree &>/dev/null
+    return $?
+}
+
+# is_screen_running returns true if GNU screen is running
+is_screen_running() {
+    [ ! -z "$STY" ]
+}
+
+# is_tmux_runnning returns true if tmux is running
+is_tmux_runnning() {
+    [ ! -z "$TMUX" ]
+}
+
+# is_screen_or_tmux_running returns true if GNU screen or tmux is running
+is_screen_or_tmux_running() {
+    is_screen_running || is_tmux_runnning
+}
+
+# shell_has_started_interactively returns true if the current shell is
+# running from command line
+shell_has_started_interactively() {
+    [ ! -z "$PS1" ]
+}
+
+is_ssh_running() {
+    [ ! -z "$SSH_CLIENT" ]
+}
+
+is_debug() {
+    if [ "$DEBUG" = 1 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+is_number() {
+    if [ $# -eq 0 ]; then
+        cat <&0
+    else
+        echo "$1"
+    fi | grep -E '^[0-9]+$' >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+alias is_int=is_number
+alias is_num=is_number
+
+ostype() {
+    # shellcheck disable=SC2119
+    uname | lower
+}
+
+os_detect() {
+    export PLATFORM
+    case "$(ostype)" in
+        *'linux'*)  PLATFORM='linux'   ;;
+        *'darwin'*) PLATFORM='osx'     ;;
+        *'bsd'*)    PLATFORM='bsd'     ;;
+        *)          PLATFORM='unknown' ;;
+    esac
+}
+
+is_osx() {
+    os_detect
+    if [ "$PLATFORM" = "osx" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+is_linux() {
+    os_detect
+    if [ "$PLATFORM" = "linux" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+is_bsd() {
+    os_detect
+    if [ "$PLATFORM" = "bsd" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+get_os() {
+    local os
+    for os in osx linux bsd; do
+        if is_$os; then
+            echo $os
+        fi
+    done
+}
+
+noecho() {
+    if [ "$(echo -n)" = "-n" ]; then
+        echo "${*:-> }\c"
+    else
+        echo -n "${@:-> }"
+    fi
+}
+
+echon() {
+    # echon - a script to emulate the -n flag functionality with 'echo' 
+    #   for Unix systems that don't have that available.
+    echo "$*" | tr -d '\n'
+}
+
+# lower returns a copy of the string with all letters mapped to their lower case.
+# shellcheck disable=SC2120
+lower() {
+    if [ $# -eq 0 ]; then
+        cat <&0
+    elif [ $# -eq 1 ]; then
+        if [ -f "$1" -a -r "$1" ]; then
+            cat "$1"
+        else
+            echo "$1"
+        fi
+    else
+        return 1
+    fi | tr "[:upper:]" "[:lower:]"
+}
+
+# upper returns a copy of the string with all letters mapped to their upper case.
+# shellcheck disable=SC2120
+upper() {
+    if [ $# -eq 0 ]; then
+        cat <&0
+    elif [ $# -eq 1 ]; then
+        if [ -f "$1" -a -r "$1" ]; then
+            cat "$1"
+        else
+            echo "$1"
+        fi
+    else
+        return 1
+    fi | tr "[:lower:]" "[:upper:]"
+}
+
+# contains returns true if $2 is within $1.
+contains() {
+    echo "$1" |
+    grep "$2" >/dev/null 2>&1
+    return $?
 }
