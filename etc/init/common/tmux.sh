044765d50cb38bc19e66491f7b219d223849c352
kegamin <keitti05@gmail.com>
2015-07-16 17:18:43 +0900

    tmux用に必要なbinを追加するscript

---
+++ etc/init/common/tmux.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. "$DOTPATH"/etc/lib/vital.sh
+
+# tmuxのヘッダーに表示するようのツール群を配置
+if [ ! -d /usr/local/bin ];then
+  e_newline
+  e_header "creating /usr/local/bin foler"
+  mkdir /usr/local/bin
+  e_done "finished."
+fi
+
+e_header "coping dotfiles/bin to /usr/local/bin"
+cp $HOME/dotfiles/bin/battery /usr/local/bin
+cp $HOME/dotfiles/bin/used_mem /usr/local/bin
+e_done "copy dotfiles/bin"
