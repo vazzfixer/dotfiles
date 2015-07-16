596f89defd279d94abdf4089e47928db9781e5dc
kegamin <keitti05@gmail.com>
2015-07-16 16:46:57 +0900

    vimのinit shell再作成

---
+++ etc/init/common/vim.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+. "$DOTPATH"/etc/lib/vital.sh
+
+e_newline
+e_header "NeoBundleInstall"
+
+vim -u $HOME/.vimrc -i NONE -c "try | NeoBundleInstall! | finally | q! | endtry" -e -s -V1
+
+e_newline
+e_arrow "installed."
--- etc/init/osx/vim.sh
+++ etc/init/osx/vim.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-# vimの最新版(clipboard対応)
-brew install vim
+. "$DOTPATH"/etc/lib/vital.sh
+
+# bundleで入れたもので、system vimを上書き
 new_vim=`ls -1 /usr/local/Cellar/vim/*/bin/vim`;
 if [ -f ${new_vim} ]; then
-  echo "linking latest vim ..."
+  e_newline
+  e_header "linking latest brewed vim ..."
   if [ ! -f /usr/bin/vim_old ]; then
     sudo mv /usr/bin/vim /usr/bin/vim_old
   fi
   sudo ln -s `ls -1 /usr/local/Cellar/vim/*/bin/vim` /usr/bin 2> /dev/null
+  e_arrow "installed."
 fi
 
-# remove outdated versions
-brew cleanup
+"$DOTPATH"/etc/init/common/vim.sh
+
