93190d7849c161f13157321cd807a41d9f7ec421
kegamin <keitti05@gmail.com>
2015-07-22 10:06:55 +0900

    linuxで起動時にエラーになったのでfix

--- etc/init/common/vim.sh
+++ etc/init/common/vim.sh
@@ -3,9 +3,20 @@
 . "$DOTPATH"/etc/lib/vital.sh
 
 e_newline
+e_header "making vimproc"
+if is_osx ; then
+  cd ~/.vim/bundle/vimproc
+  make -f make_mac.mak
+elif is_linux ; then
+  cd ~/.vim/bundle/vimproc
+  make -f make_unix.mak
+else
+  e_error "get_os failed. Ignore making vimproc."
+fi
+
+e_newline
 e_header "NeoBundleInstall"
 
 vim -u $HOME/.vimrc -i NONE -c "try | NeoBundleInstall! | finally | q! | endtry" -e -s -V1
 
-e_newline
 e_done "installed."
