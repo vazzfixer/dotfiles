412824878fc6f2db96f1fbd4ffc4bc14dd19b7ca
kegamin <keitti05@gmail.com>
2015-07-22 10:11:57 +0900

    エラー時のメッセージ追加

--- etc/init/common/tmux.sh
+++ etc/init/common/tmux.sh
@@ -7,10 +7,15 @@ if [ ! -d /usr/local/bin ];then
   e_newline
   e_header "creating /usr/local/bin folder"
   mkdir /usr/local/bin
-  e_done "finished."
+  e_success "/usr/local/bin folder"
 fi
 
 e_header "coping dotfiles/bin to /usr/local/bin"
-cp $HOME/dotfiles/bin/battery /usr/local/bin
-cp $HOME/dotfiles/bin/used_mem /usr/local/bin
-e_done "copy dotfiles/bin"
+sudo cp $HOME/dotfiles/bin/battery /usr/local/bin
+sudo cp $HOME/dotfiles/bin/used_mem /usr/local/bin
+
+if [ ! -x /usr/local/bin/battery ]; then
+  e_failure "coping dotfiles/bin"
+fi
+
+e_done "coping dotfiles/bin"
