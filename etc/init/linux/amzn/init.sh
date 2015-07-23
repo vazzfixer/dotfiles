73e090d336305fb968cfb263ed9a0bee2138e07a
kegamin <keitti05@gmail.com>
2015-07-23 10:54:18 +0900

    ディストリ毎に実行するファイルを変更できるように修正

--- etc/init/linux/amzn/init.sh
+++
@@ -1,4 +0,0 @@
-sudo yum install gcc
-sudo yum install tmux
-sudo yum install zsh
-chsh -s $(which zsh)
---
+++ etc/init/linux/init.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+. "$DOTPATH"/etc/lib/vital.sh
+
+dist=get_dist
+if [ "$dist" = "Amazon" ]; then
+  install_yum_package
+  install_dist_files
+elif [ "$dist" = "CentOS" ]; then
+  install_yum_package
+else
+  e_error "unsupported distribution. install common settings only."
+fi
+
+change_default_to_zsh
+
+install_yum_package() {
+  e_header "install yum packages"
+  sudo yum install gcc
+  sudo yum install tmux
+  sudo yum install zsh
+  e_success "install yum packages"
+}
+
+change_default_to_zsh() {
+  e_header "changing default shell to zsh"
+  chsh -s $(which zsh)
+}
+
+install_dist_files() {
+  for i in "$DOTPATH"/etc/init/"$(get_os)"/"$(get_dist)"/*[^init].sh
+  do
+    if [ -f "$i" ]; then
+      e_arrow "$(basename "$i")"
+      bash "$i"
+    else
+      continue
+    fi
+  done
+}
