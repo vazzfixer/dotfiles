e8d52bd277d73f79b7460d9781be797b8642646e
kegamin <keitti05@gmail.com>
2015-07-23 12:49:58 +0900

    userが撮れてなかった

--- etc/init/linux/Amazon/newuser.sh
+++ etc/init/linux/Amazon/newuser.sh
@@ -10,39 +10,39 @@ fi
 
 e_header "Creating new user..."
 e_prompt "Input a new username"
-read new
+read NEWUSER
 
-user=`cut -d':' -f1 /etc/passwd | grep $new`
-if [ -n "$user" ]; then
-  e_error "$user is already added. skip to add the user."
+check=`cut -d':' -f1 /etc/passwd | grep $NEWUSER`
+if [ -n "$check" ]; then
+  e_error "$NEWUSER is already added. skip to add the user."
   exit 0;
 fi
 
-sudo adduser $user
-sudo usermod -G wheel $user
-sudo rsync -a ~/.ssh/authorized_keys /home/$user/.ssh/
-sudo chown -R $user:$user /home/$user/.ssh
-sudo chmod -R go-rwx /home/$user/.ssh
+sudo adduser $NEWUSER
+sudo usermod -G wheel $NEWUSER
+sudo rsync -a ~/.ssh/authorized_keys /home/$NEWUSER/.ssh/
+sudo chown -R $NEWUSER:$NEWUSER /home/$NEWUSER/.ssh
+sudo chmod -R go-rwx /home/$NEWUSER/.ssh
 
 sudoers_file=/etc/sudoers.d/user
 cat << EOF | sudo tee $sudoers_file >/dev/null
-$user ALL = NOPASSWD: ALL
+$NEWUSER ALL = NOPASSWD: ALL
 
-# User rules for $user
-$user ALL=(ALL) NOPASSWD:ALL
+# User rules for $NEWUSER
+$NEWUSER ALL=(ALL) NOPASSWD:ALL
 EOF
 
 if [ ! -f $sudoers_file ]; then
   e_failure "sudoersファイルの作成に失敗しました."
 else
-  e_success "sudoers file generated for $user"
+  e_success "sudoers file generated for $NEWUSER"
 fi
 
 e_header "checking sudo command..."
-if [ `sudo -u $user whoami` != $user ]; then
-  e_failure "creating $user failed. chech errors..."
-elif [ `sudo -u $user sudo whoami` != "root" ]; then
-  e_failure "$user can't use sudo command. check errors."
+if [ `sudo -u $NEWUSER whoami` != $NEWUSER ]; then
+  e_failure "creating $NEWUSER failed. chech errors..."
+elif [ `sudo -u $NEWUSER sudo whoami` != "root" ]; then
+  e_failure "$NEWUSER can't use sudo command. check errors."
 fi
 e_success "sudo command succeeded. ready to delete ec2-user"
 
@@ -57,4 +57,4 @@ if [ -f /etc/ssh/sshd_config ]; then
   fi
 fi
 
-e_done "installed $user user. DELETE ec2-user if needed."
+e_done "installed $NEWUSER user. DELETE ec2-user if needed."
