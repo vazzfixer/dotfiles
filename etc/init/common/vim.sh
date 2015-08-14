c6ead8a68fa994eaff0300209e737a24c2d0cf8c
kegamin <keitti05@gmail.com>
2015-08-14 15:56:36 +0900

    vimprocのディレクトリを変えたので合わせて修正

--- etc/init/common/vim.sh
+++ etc/init/common/vim.sh
@@ -5,9 +5,9 @@
 e_newline
 e_header "making vimproc"
 if is_osx ; then
-  make -C $HOME/.vim/bundle/vimproc.vim -f make_mac.mak
+  make -C $HOME/.vim/bundle/vimproc -f make_mac.mak
 elif is_linux ; then
-  make -C $HOME/.vim/bundle/vimproc.vim -f make_unix.mak
+  make -C $HOME/.vim/bundle/vimproc -f make_unix.mak
 else
   e_error "get_os failed. Ignore making vimproc."
 fi
